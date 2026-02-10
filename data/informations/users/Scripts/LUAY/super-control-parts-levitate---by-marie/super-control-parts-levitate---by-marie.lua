8d7299619eaf806fdfd94f17fe52aa25011480d4
higuysdorobloxjoaopk-maker <higuysdorobloxjoaopk@gmail.com>
2026-02-10 20:05:52 -0300

    Rename super-control-parts-levitate---by-marie.lua to super-control-parts-levitate---by-marie.lua

---
+++ data/informations/users/Scripts/LUAY/super-control-parts-levitate---by-marie/super-control-parts-levitate---by-marie.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vitorbing34-cpu/Levantar-blocos-tentativa-17/main/script.lua"))()
--- data/informations/users/Scripts/joao009653/super-control-parts-levitate---by-marie/super-control-parts-levitate---by-marie.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vitorbing34-cpu/Levantar-blocos-tentativa-17/main/script.lua"))()
